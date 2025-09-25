7bdb1c603741cb53a2744c55dfb484fafd6b3f30
hiroset0391162621 <takashi.hirose.b6@tohoku.ac.jp>
2025-09-25 22:15:04 +0900

    update

--- example/calcNIP_example_120s.py
+++ example/calcNIP_example_120s.py
@@ -12,6 +12,9 @@ import backazimuth
 sys.path.append(sys.path.append("utils/"))
 import circular
 import bootstrap    
+sys.path.append(sys.path.append("nipfilter/"))
+from core import stransform, istransform
+import filter as filt_par
 
 try:
     import scienceplots
@@ -275,12 +278,13 @@ def plot_bazmean(windL, Tv, Fv, mean_direction_ci):
 
 if __name__ == '__main__':
 
+    polarization = 'retrograde'
     Fs = 20.0 ### sampling frequency [Hz]
-    azimuth = 40.0 ### initial value. does not affect the final result.
+    azimuth = 220.0 ### propagating direction of Rayleigh waves (crater->seismometer)
     windL = 120.0 ### time window length [sec]
     station = 'V.KIRA'
-    starttime = datetime.datetime(2017,1,1,1,10,0)
-    #starttime = datetime.datetime(2017,10,15,1,10,0)
+    #starttime = datetime.datetime(2017,1,1,1,10,0)
+    starttime = datetime.datetime(2017,10,15,1,10,0)
 
     stream_Z = obspy.read('sac/'+starttime.strftime("%Y%m%d")+'/'+starttime.strftime("%Y%m%d%H")+'00'+station+'.U.sac').resample(Fs, window='hann')
     stream_N = obspy.read('sac/'+starttime.strftime("%Y%m%d")+'/'+starttime.strftime("%Y%m%d%H")+'00'+station+'.N.sac').resample(Fs, window='hann')
@@ -299,26 +303,112 @@ if __name__ == '__main__':
     tr_Z = stream_Z[0].data
     tr_N = stream_N[0].data
     tr_E = stream_E[0].data
-
-
-    """
-    Back azimuth estimation
-    """
-    baz, nip, Tv, Fv, Sv, Se, Sn = backazimuth.calc_baz(tr_Z.copy(), tr_E.copy(), tr_N.copy(), Fs, azimuth, 'retrograde')
-
-    plot_baz(windL, tr_Z, Fv, Tv, Sv, baz, nip)
-
-
     
     """
-    bootstrap
-    Note: Applying the bootstrap method to all f is very computationally expensive.
+    Stockwell transform
     """
-    mean_direction_ci = ci_eachf(Fv, baz, Tv)
+    Sv, Tv, Fv = stransform(tr_Z, Fs, return_time_freq=True)
+    Se, _, _ = stransform(tr_E, Fs, return_time_freq=True)
+    Sn, _, _ = stransform(tr_N, Fs, return_time_freq=True)
     
-    plot_bazmean(windL, Tv, Fv, mean_direction_ci)
+    """
+    Rotate horizontal components NE -> RT
+    """
+    Sr, St = filt_par.rotate_NE_RT(Sn, Se, azimuth)
     
-        
-    
+    """
+    Shift vertical component and Calculate NIP(t,f)
+    """
+    Sv_shifted = filt_par.get_shift(polarization) * Sv
+    nip = filt_par.NIP(Sr, Sv_shifted)
+    nip_mean = np.nanmean(nip, axis=1) ### averege over time
+    nip_eightyper = np.percentile(nip, 80, axis=1) ### 80% percentile over time
 
+    """
+    Plot
+    """
+    x_lim = 0.12
+    fig = plt.figure(figsize=(6,8)) 
+    ax1 = plt.axes([x_lim,0.82,0.68,0.15])
+    trace_amp = 1.5*np.max(np.abs(tr_Z))
+    ax1.plot(np.linspace(0,windL, len(tr_Z)), tr_Z, lw=1, color='k')
+    ax1.annotate('Z (raw)', xy=(0.05, 0.95), xycoords='axes fraction', fontsize=12,
+                horizontalalignment='left', verticalalignment='top')
+    ax1.set_xlim(0,windL)
+    ax1.set_ylim(-trace_amp, trace_amp)
+    ax1.set_ylabel('velocity [m/s]', fontsize=12)
+
+    ax11 = plt.axes([0.9,0.82,0.1,0.2])
+    plt.xticks(())
+    plt.yticks(())
+    ax11.spines['right'].set_color('none')
+    ax11.spines['left'].set_color('none')
+    ax11.spines['top'].set_color('none')
+    ax11.spines['bottom'].set_color('none')   
+    ax11.patch.set_alpha(0.)
+
+
+
+    ax5 = plt.axes([x_lim,0.55,0.85,0.2])
+    SC = ax5.pcolormesh(Tv, Fv, np.abs(Sv), cmap=plt.cm.jet, rasterized=True)
+    ax5.set_xlim(0,windL)
+    ax5.set_ylim(0.05,10)
+    ax5.set_yscale('log')
+    ax5.set_ylabel('Frequency [Hz]', fontsize=12)
+
+    ax6 = plt.axes([0.9,0.55,0.1,0.2])
+    plt.xticks(())
+    plt.yticks(())
+    ax6.spines['right'].set_color('none')
+    ax6.spines['left'].set_color('none')
+    ax6.spines['top'].set_color('none')
+    ax6.spines['bottom'].set_color('none')   
+    ax6.patch.set_alpha(0.)
+    cbar=plt.colorbar(SC, pad=0.05, orientation='vertical')
+    cbar.set_label(r'$|S_v(\tau, f)|$ [m/s]', fontsize=12)
+    cbar.ax.tick_params(labelsize=12)
+
+
+    ax3 = plt.axes([x_lim,0.3,0.85,0.2])      
+    import matplotlib as mpl
+    cmap = plt.cm.bwr
+    cmaplist = [cmap(i) for i in range(cmap.N)]
+    cmap = mpl.colors.LinearSegmentedColormap.from_list(
+        'Custom cmap', cmaplist, cmap.N)
+    bounds = np.arange(-1, 1.25, 0.25) 
+    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+    SC = ax3.pcolormesh(Tv, Fv, nip, cmap=cmap, norm=norm, rasterized=True)
+
+
+    ax3.set_xlim(0,windL)
+    ax3.set_ylim(0.05,10)
+    ax3.set_yscale('log')
+    ax3.set_xlabel('lapse time [s]', fontsize=12)
+    ax3.set_ylabel('Frequency [Hz]', fontsize=12)
+
+    ax4 = plt.axes([0.9,0.3,0.1,0.2])
+    #ax4.xaxis.set_major_formatter(FormatStrFormatter("%.2f"))
+    plt.xticks(())
+    plt.yticks(())
+    ax4.spines['right'].set_color('none')
+    ax4.spines['left'].set_color('none')
+    ax4.spines['top'].set_color('none')
+    ax4.spines['bottom'].set_color('none')   
+    ax4.patch.set_alpha(0.)
+    cbar=plt.colorbar(SC, pad=0.05, orientation='vertical', ticks=np.arange(-1, 1.25, 0.25))
+    cbar.set_label('NIP', fontsize=12)
+    cbar.ax.tick_params(labelsize=12)
     
+    
+    ax5 = plt.axes([x_lim,0.07,0.3,0.15])   
+    ax5.plot(nip_mean, Fv[:,0], lw=1.5, ls='--', color='C0', label='NIP (mean)', zorder=2)
+    ax5.plot(nip_eightyper, Fv[:,0], lw=1.5, color='C1', label='NIP (80th percentile)', zorder=2)
+    ax5.set_ylim(0.05,10)
+    ax5.set_yscale('log')
+    ax5.set_xlabel('NIP', fontsize=12)
+    ax5.set_ylabel('Frequency [Hz]', fontsize=12)
+    ax5.legend(fontsize=12, bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, )
+    ax5.set_xlim(-1,1)
+    
+    
+    plt.show()   
