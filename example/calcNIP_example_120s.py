8865fec1b42ec575ad932e8334b7875c9c2ac505
hiroset0391162621 <takashi.hirose.b6@tohoku.ac.jp>
2025-09-25 22:27:57 +0900

    update

--- example/calcNIP_example_120s.py
+++ example/calcNIP_example_120s.py
@@ -6,274 +6,99 @@ from obspy.core import UTCDateTime
 import matplotlib.pyplot as plt
 from matplotlib.ticker import *
 import warnings
-warnings.simplefilter('ignore')
-sys.path.append(sys.path.append("core/"))
-import backazimuth
-sys.path.append(sys.path.append("utils/"))
-import circular
-import bootstrap    
+warnings.simplefilter('ignore') 
 sys.path.append(sys.path.append("nipfilter/"))
 from core import stransform, istransform
 import filter as filt_par
 
-try:
-    import scienceplots
-except:
-    pass
-plt.style.use(['science', 'nature'])
-plt.rcParams['xtick.labelsize'] = 12
-plt.rcParams['ytick.labelsize'] = 12
-plt.rcParams['axes.linewidth'] = 1.5 
-plt.rcParams['xtick.major.width'] = 1.5
-plt.rcParams['ytick.major.width'] = 1.5
-plt.rcParams['xtick.minor.width'] = 1.5
-plt.rcParams['ytick.minor.width'] = 1.5
-plt.rcParams['xtick.direction'] = "inout"
-plt.rcParams['ytick.direction'] = "inout"
-plt.rcParams['xtick.major.size'] = 6.0
-plt.rcParams['xtick.minor.size'] = 4.0
-plt.rcParams['ytick.major.size'] = 6.0
-plt.rcParams['ytick.minor.size'] = 4.0
-plt.rcParams['xtick.major.pad']='8'
-plt.rcParams['xtick.top'] = True
-plt.rcParams['ytick.right'] = True
-plt.rcParams['axes.edgecolor'] = '#08192D' 
-plt.rcParams['axes.labelcolor'] = '#08192D' 
-plt.rcParams['xtick.color'] = '#08192D' 
-plt.rcParams['ytick.color'] = '#08192D'
-plt.rcParams['text.color'] = '#08192D' 
-plt.rcParams['legend.framealpha'] = 1.0 
-plt.rcParams['pdf.fonttype'] = 42
-plt.rcParams['text.usetex']   = False
-
-
-
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-import matplotlib.colors as colors
+from typing import Optional, Tuple, Union, List, Any, Dict, Callable, TypeVar, cast
 import warnings
 warnings.filterwarnings('ignore')
 
+try:
+    import scienceplots  # type: ignore
+except:
+    pass
 
-gscolors = np.array(
-    [
-        [243, 243, 241, 1],
-        [0, 242, 242, 1],
-        [0, 121, 242, 1],
-        [0, 0, 242, 1],
-        [121, 0, 242, 1],
-        [242, 0, 242, 1],
-        [242, 0, 121, 1],
-        [242, 0, 0, 1],
-        [242, 121, 0, 1],
-        [242, 242, 0, 1],
-        [121, 242, 0, 1],
-        [0, 242, 0, 1],
-        [0, 242, 121, 1],
-        [0, 242, 242, 1],
-        [243, 243, 241, 1],
-    ],
-    dtype=np.float64,
-)
-gscolors[:, :3] /= 256  
-gscmap = ListedColormap(gscolors)
-hsv2 = LinearSegmentedColormap.from_list("gscmap2", colors=gscolors)
+# GMT-style matplotlib settings
+try:
+    plt.style.use(["science", "nature"])
+except:
+    plt.style.use("default")  # Fallback to default style
 
+# GMT-style color and font settings
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = ['Helvetica', 'Arial', 'DejaVu Sans']
+plt.rcParams['font.size'] = 10
+plt.rcParams['figure.facecolor'] = 'white'
+plt.rcParams['axes.facecolor'] = 'white'
 
-def ci_eachf(Fv, baz, Tv):
-    time_baz_idx = np.where( (Tv[0,:]>=0) & (Tv[0,:]<=120) )[0]
-    mean_direction_ci = np.zeros((Fv.shape[0], 3))*np.nan
-    for i in range(Fv.shape[0]): 
-        baz2 = (baz[:,time_baz_idx][i,:]).flatten()
-        baz2 = baz2[baz2==baz2]
+# GMT-style tick and axis settings
+plt.rcParams['xtick.direction'] = "out"
+plt.rcParams['ytick.direction'] = "out"
+plt.rcParams["text.usetex"] = False
+plt.rcParams['agg.path.chunksize'] = 100000
+plt.rcParams["date.converter"] = "concise"
+
+# Tick parameters (GMT-like style)
+plt.rcParams['xtick.labelsize'] = 11
+plt.rcParams['ytick.labelsize'] = 11
+plt.rcParams['axes.linewidth'] = 1.0  # GMT uses thinner lines
+plt.rcParams['xtick.major.width'] = 1.0
+plt.rcParams['ytick.major.width'] = 1.0
+plt.rcParams['xtick.minor.width'] = 0.5
+plt.rcParams['ytick.minor.width'] = 0.5
+plt.rcParams['xtick.major.size'] = 4.0
+plt.rcParams['xtick.minor.size'] = 2.0
+plt.rcParams['ytick.major.size'] = 4.0
+plt.rcParams['ytick.minor.size'] = 2.0
+
+# GMT-style colors
+plt.rcParams['axes.edgecolor'] = 'black'
+plt.rcParams['axes.labelcolor'] = 'black'
+plt.rcParams['xtick.color'] = 'black'
+plt.rcParams['ytick.color'] = 'black'
+
+# Grid settings (GMT-like)
+plt.rcParams['axes.grid'] = False  # We'll add custom grid
+plt.rcParams['grid.color'] = 'gray'
+plt.rcParams['grid.linestyle'] = ':'
+plt.rcParams['grid.linewidth'] = 0.5
+plt.rcParams['grid.alpha'] = 0.7
+
+def apply_gmt_style_to_axis(ax: Any, grid: bool = True, minor_ticks: bool = True) -> Any:
+    """
+    Apply GMT-style formatting to matplotlib axis
+    
+    Args:
+        ax: Matplotlib axis
+        grid: Whether to show grid
+        minor_ticks: Whether to show minor ticks
         
-        
-        
-        if Fv[i,0]>=0.0:
-            mean_direction_low, mean_direction_high, kappa_low, kappa_high = bootstrap.bootstrap_vm_confidence_interval(np.deg2rad(baz2))   
-            mean_direction_low = np.rad2deg(mean_direction_low)
-            mean_direction_high = np.rad2deg(mean_direction_high)
-            print(str(Fv[i,0])+"Hz", mean_direction_low, mean_direction_high)
-            
-            if mean_direction_low<-180:
-                mean_direction_low = np.nan
-            
-            if mean_direction_high>180:
-                mean_direction_high = np.nan
-                
-            mean_direction_ci[i,0] = Fv[i,0]
-            mean_direction_ci[i,1] = mean_direction_low
-            mean_direction_ci[i,2] = mean_direction_high
-        
-
-    return mean_direction_ci
-
-def plot_baz(windL, tr_Z, Fv, Tv, Sv, baz, nip):
-    fig = plt.figure(figsize=(5,8)) 
-
-
-    ax1 = plt.axes([0.1,0.78,0.68,0.15])
-    trace_amp = 1.5*np.max(np.abs(tr_Z))
-    ax1.plot(np.linspace(0,windL, len(tr_Z)), tr_Z, lw=1, color='k')
-    ax1.annotate(station+'.U (raw)', xy=(0.05, 0.95), xycoords='axes fraction', fontsize=12,
-                horizontalalignment='left', verticalalignment='top')
-    ax1.set_xlim(0,windL)
-    ax1.set_ylim(-trace_amp, trace_amp)
-    ax1.set_ylabel('velocity [m/s]', fontsize=12)
-
-    ax11 = plt.axes([0.9,0.5,0.1,0.2])
-    plt.xticks(())
-    plt.yticks(())
-    ax11.spines['right'].set_color('none')
-    ax11.spines['left'].set_color('none')
-    ax11.spines['top'].set_color('none')
-    ax11.spines['bottom'].set_color('none')   
-    ax11.patch.set_alpha(0.)
-
-
-
-    ax5 = plt.axes([0.1,0.5,0.85,0.2])
-    SC = ax5.pcolormesh(Tv, Fv, np.abs(Sv), cmap=plt.cm.jet, rasterized=True)
-    ax5.set_xlim(0,windL)
-    ax5.set_ylim(0.05,10)
-    ax5.set_yscale('log')
-    ax5.set_ylabel('Frequency [Hz]', fontsize=12)
-
-    ax6 = plt.axes([0.9,0.5,0.1,0.2])
-    #ax4.xaxis.set_major_formatter(FormatStrFormatter("%.2f"))
-    plt.xticks(())
-    plt.yticks(())
-    ax6.spines['right'].set_color('none')
-    ax6.spines['left'].set_color('none')
-    ax6.spines['top'].set_color('none')
-    ax6.spines['bottom'].set_color('none')   
-    ax6.patch.set_alpha(0.)
-    cbar=plt.colorbar(SC, pad=0.05, orientation='vertical')
-    cbar.set_label(r'$|S_v(\tau, f)|$ [m/s]', fontsize=12)
-    cbar.ax.tick_params(labelsize=12)
-
-
-    ax2 = plt.axes([0.1,0.25,0.85,0.2])      
-    bounds = [-30, -20, -10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
-    norm = colors.BoundaryNorm(boundaries=bounds, ncolors=256)
-    SC = ax2.pcolormesh(Tv, Fv, baz, cmap=hsv2, norm=norm, rasterized=True)
-
-
-    freq = Fv[:,0]
-    freq_low, freq_high = 1.3, 3
-    i_freq_low = np.argmin(np.abs(freq - freq_low))
-    i_freq_high = np.argmin(np.abs(freq - freq_high))
-    print('frequency', Fv)
-    print('df', np.diff(freq))
-    print('time', Tv[0,:])
-    print('dt', np.diff(Tv[0,:]))
-
-    ax2.set_xlim(0,windL)
-    ax2.set_ylim(0.05,10)
-    ax2.set_yscale('log')
-    #ax2.set_xlabel('lapse time [s]', fontsize=12)
-    ax2.set_ylabel('Frequency [Hz]', fontsize=12)
-
-
-    ax4 = plt.axes([0.9,0.25,0.1,0.2])
-    #ax4.xaxis.set_major_formatter(FormatStrFormatter("%.2f"))
-    plt.xticks(())
-    plt.yticks(())
-    ax4.spines['right'].set_color('none')
-    ax4.spines['left'].set_color('none')
-    ax4.spines['top'].set_color('none')
-    ax4.spines['bottom'].set_color('none')   
-    ax4.patch.set_alpha(0.)
-    cbar=plt.colorbar(SC, pad=0.05, orientation='vertical')
-    cbar.set_label(r'Back Azimuth [$^{\circ}$]', fontsize=12)
-    cbar.ax.tick_params(labelsize=12)
-
-
-
-    ax3 = plt.axes([0.1,0.0,0.85,0.2])
-    import matplotlib as mpl
-    cmap = plt.cm.binary
-    cmaplist = [cmap(i) for i in range(cmap.N)]
-    cmap = mpl.colors.LinearSegmentedColormap.from_list(
-        'Custom cmap', cmaplist, cmap.N)
-    bounds = np.arange(0.8, 1.02, 0.02) 
-    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-    SC = ax3.pcolormesh(Tv, Fv, nip, cmap=cmap, norm=norm, rasterized=True)
-
-
-    ax3.set_xlim(0,windL)
-    ax3.set_ylim(0.05,10)
-    ax3.set_yscale('log')
-    ax3.set_xlabel('lapse time [s]', fontsize=12)
-    ax3.set_ylabel('Frequency [Hz]', fontsize=12)
-
-    ax4 = plt.axes([0.9,0.0,0.1,0.2])
-    #ax4.xaxis.set_major_formatter(FormatStrFormatter("%.2f"))
-    plt.xticks(())
-    plt.yticks(())
-    ax4.spines['right'].set_color('none')
-    ax4.spines['left'].set_color('none')
-    ax4.spines['top'].set_color('none')
-    ax4.spines['bottom'].set_color('none')   
-    ax4.patch.set_alpha(0.)
-    cbar=plt.colorbar(SC, pad=0.05, orientation='vertical', ticks=[0.4,0.5,0.6,0.7,0.8,0.9,1], extend='min')
-    cbar.set_label('NIP', fontsize=12)
-    cbar.ax.tick_params(labelsize=12)
-
-    plt.suptitle(starttime.strftime("%Y.%m.%d %H:%M:%S")+'-'+(starttime+datetime.timedelta(seconds=windL)).strftime(" %H:%M:%S"), fontsize=14)
-
+    Returns:
+        Formatted axis
+    """
+    # GMT-style frame
+    for spine in ax.spines.values():
+        spine.set_linewidth(1.0)
+        spine.set_color('black')
     
-    plt.show()
+    # GMT-style ticks
+    ax.tick_params(axis='x', which='major', labelsize=13, width=1.0, length=4.0, direction='out', color='black')
+    ax.tick_params(axis='y', which='major', labelsize=14, width=1.0, length=4.0, direction='out', color='black')
+    if minor_ticks:
+        ax.tick_params(axis='both', which='minor', width=0.5, length=2.0, direction='out', color='black')
     
+    # GMT-style grid
+    if grid:
+        ax.grid(True, which='major', color='gray', linestyle=':', linewidth=0.5, alpha=0.7)
+        ax.set_axisbelow(True)
     
-def plot_bazmean(windL, Tv, Fv, mean_direction_ci):
-    time_baz_idx = np.where( (Tv[0,:]>=0) & (Tv[0,:]<=windL) )[0]
-    mean_baz2 = np.zeros(Fv.shape[0])*np.nan
-    Results = np.zeros((Fv.shape[0],2))
-    for i in range(Fv.shape[0]):
-        baz2 = (baz[:,time_baz_idx][i,:]).flatten()
-        baz2 = baz2[baz2==baz2]
-        circmean_val = circular.mean(baz2, deg=True)
-        if circmean_val>180.0:
-            circmean_val -= 360.0
-        mean_baz2[i] = circmean_val
-        Results[i] = np.array([Fv[i,0], circmean_val])
-
-    fv = Results[:,0]
-    x = Results[:,1]
-
-    fig = plt.figure(figsize=(5,8)) 
-    ax3 = plt.axes([0.1,0.68,0.4,0.25])
-    plt.plot(x, fv, lw=1.5, color='C0', zorder=2)
+    # White background
+    ax.set_facecolor('white')
     
-    for i in range(len(mean_direction_ci[:,0])):
-        plt.plot([mean_direction_ci[i,1], mean_direction_ci[i,2]], [mean_direction_ci[i,0], mean_direction_ci[i,0]], color='pink', zorder=1)
-    
-    plt.axvline(x=30.0, color='red', ls='--', zorder=3) ### V.KIRA -> Shinmoe 
-    plt.axvline(x=50.0, color='red', ls='--', zorder=3) ### V.KIRA -> Shinmoe 
-    plt.yscale('log')
-    plt.xlim(-180,180)
-    plt.xticks([-180,-90,0,90,180])
-    plt.ylim(0.05,10)
-    ax3.set_xlabel(r'mean Back Azimuth [$^{\circ}$]', fontsize=12)
-    ax3.set_ylabel('Frequency [Hz]', fontsize=12)
-    ax3.annotate('Shinmoe-dake', xy=(40,10), xycoords='data', fontsize=10, va='bottom', ha='center', xytext=(0, 3), textcoords='offset points', color='red')
+    return ax
 
-
-    ax4 = plt.axes([0.65,0.73,0.2,0.2])
-    ax4.plot(x, fv, lw=1.5, color='C0')
-    for i in range(len(mean_direction_ci[:,0])):
-        plt.plot([mean_direction_ci[i,1], mean_direction_ci[i,2]], [mean_direction_ci[i,0], mean_direction_ci[i,0]], color='pink', zorder=1)
-    ax4.set_yscale('log')
-    ax4.set_xlim(30,50)
-    ax4.set_ylim(1,3)
-    #ax4.set_xlabel(r'mean Back Azimuth [$^{\circ}$]', fontsize=10)
-
-
-    plt.suptitle(starttime.strftime("%Y.%m.%d %H:%M:%S")+'-'+(starttime+datetime.timedelta(seconds=windL)).strftime(" %H:%M:%S"), fontsize=14)
-        
-    plt.show()
-
-    
 
 
 if __name__ == '__main__':
@@ -327,8 +152,14 @@ if __name__ == '__main__':
     """
     Plot
     """
+    # 図のアスペクト比を柔軟に変更できるように設定
+    # デフォルトは (width=6, height=8) だが、必要に応じて変更可能
+    figure_width = 6    # 図の幅 [インチ]
+    figure_height = 8   # 図の高さ [インチ]
+    figsize = (figure_width, figure_height)
+    
     x_lim = 0.12
-    fig = plt.figure(figsize=(6,8)) 
+    fig = plt.figure(figsize=figsize) 
     ax1 = plt.axes([x_lim,0.82,0.68,0.15])
     trace_amp = 1.5*np.max(np.abs(tr_Z))
     ax1.plot(np.linspace(0,windL, len(tr_Z)), tr_Z, lw=1, color='k')
@@ -336,7 +167,7 @@ if __name__ == '__main__':
                 horizontalalignment='left', verticalalignment='top')
     ax1.set_xlim(0,windL)
     ax1.set_ylim(-trace_amp, trace_amp)
-    ax1.set_ylabel('velocity [m/s]', fontsize=12)
+    ax1.set_ylabel('', fontsize=14)
 
     ax11 = plt.axes([0.9,0.82,0.1,0.2])
     plt.xticks(())
@@ -354,7 +185,7 @@ if __name__ == '__main__':
     ax5.set_xlim(0,windL)
     ax5.set_ylim(0.05,10)
     ax5.set_yscale('log')
-    ax5.set_ylabel('Frequency [Hz]', fontsize=12)
+    ax5.set_ylabel('Frequency [Hz]', fontsize=14)
 
     ax6 = plt.axes([0.9,0.55,0.1,0.2])
     plt.xticks(())
@@ -365,7 +196,7 @@ if __name__ == '__main__':
     ax6.spines['bottom'].set_color('none')   
     ax6.patch.set_alpha(0.)
     cbar=plt.colorbar(SC, pad=0.05, orientation='vertical')
-    cbar.set_label(r'$|S_v(\tau, f)|$ [m/s]', fontsize=12)
+    cbar.set_label(r'$|S_v(\tau, f)|$ [m/s]', fontsize=14)
     cbar.ax.tick_params(labelsize=12)
 
 
@@ -383,8 +214,8 @@ if __name__ == '__main__':
     ax3.set_xlim(0,windL)
     ax3.set_ylim(0.05,10)
     ax3.set_yscale('log')
-    ax3.set_xlabel('lapse time [s]', fontsize=12)
-    ax3.set_ylabel('Frequency [Hz]', fontsize=12)
+    ax3.set_xlabel('lapse time [s]', fontsize=14)
+    ax3.set_ylabel('Frequency [Hz]', fontsize=14)
 
     ax4 = plt.axes([0.9,0.3,0.1,0.2])
     #ax4.xaxis.set_major_formatter(FormatStrFormatter("%.2f"))
@@ -396,19 +227,26 @@ if __name__ == '__main__':
     ax4.spines['bottom'].set_color('none')   
     ax4.patch.set_alpha(0.)
     cbar=plt.colorbar(SC, pad=0.05, orientation='vertical', ticks=np.arange(-1, 1.25, 0.25))
-    cbar.set_label('NIP', fontsize=12)
+    cbar.set_label('NIP', fontsize=14)
     cbar.ax.tick_params(labelsize=12)
     
     
-    ax5 = plt.axes([x_lim,0.07,0.3,0.15])   
-    ax5.plot(nip_mean, Fv[:,0], lw=1.5, ls='--', color='C0', label='NIP (mean)', zorder=2)
-    ax5.plot(nip_eightyper, Fv[:,0], lw=1.5, color='C1', label='NIP (80th percentile)', zorder=2)
-    ax5.set_ylim(0.05,10)
-    ax5.set_yscale('log')
-    ax5.set_xlabel('NIP', fontsize=12)
-    ax5.set_ylabel('Frequency [Hz]', fontsize=12)
-    ax5.legend(fontsize=12, bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, )
-    ax5.set_xlim(-1,1)
+    ax7 = plt.axes([x_lim,0.07,0.3,0.15])   
+    ax7.plot(nip_mean, Fv[:,0], lw=1.5, ls='--', color='C0', label='NIP (mean)', zorder=2)
+    ax7.plot(nip_eightyper, Fv[:,0], lw=1.5, color='C1', label='NIP (80th percentile)', zorder=2)
+    ax7.set_ylim(0.05,10)
+    ax7.set_yscale('log')
+    ax7.set_xlabel('NIP', fontsize=14)
+    ax7.set_ylabel('Frequency [Hz]', fontsize=14)
+    ax7.legend(fontsize=14, bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, )
+    ax7.set_xlim(-1,1)
     
     
-    plt.show()   
+    ax1 = apply_gmt_style_to_axis(ax1, grid=False, minor_ticks=True)
+    ax3 = apply_gmt_style_to_axis(ax3, grid=False, minor_ticks=True)
+    ax5 = apply_gmt_style_to_axis(ax5, grid=True, minor_ticks=True)
+    ax7 = apply_gmt_style_to_axis(ax7, grid=True, minor_ticks=True)
+
+    plt.savefig("example.pdf", dpi=300, bbox_inches='tight')
+    plt.savefig("example.png", dpi=300, bbox_inches='tight')
+    plt.show()
